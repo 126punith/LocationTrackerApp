808b8103e3ee87a771946a54da447efada826143
Punith M <punithm300@gmail.com>
2022-09-13 21:44:48 +0530

    opencage-api-client package installed and location api used to get lat and longitude

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -5,25 +5,24 @@
  */
 import { FontAwesome } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { ColorSchemeName, Pressable } from 'react-native';
+import { Pressable } from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import ModalScreen from '../screens/ModalScreen';
-import NotFoundScreen from '../screens/NotFoundScreen';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
-import LinkingConfiguration from './LinkingConfiguration';
+import {
+  RootStackParamList,
+  RootTabParamList,
+  RootTabScreenProps,
+} from '../types';
 
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+export default function Navigation() {
   return (
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <NavigationContainer>
       <RootNavigator />
     </NavigationContainer>
   );
@@ -38,11 +37,11 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 function RootNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-      <Stack.Group screenOptions={{ presentation: 'modal' }}>
-        <Stack.Screen name="Modal" component={ModalScreen} />
-      </Stack.Group>
+      <Stack.Screen
+        name='Root'
+        component={BottomTabNavigator}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
@@ -54,30 +53,32 @@ function RootNavigator() {
 const BottomTab = createBottomTabNavigator<RootTabParamList>();
 
 function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
-
   return (
     <BottomTab.Navigator
-      initialRouteName="TabOne"
+      initialRouteName='TabOne'
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme].tint,
-      }}>
+        tabBarActiveTintColor: Colors.tint,
+      }}
+    >
       <BottomTab.Screen
-        name="TabOne"
+        name='TabOne'
         component={TabOneScreen}
         options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
           title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name='location-arrow' color={color} />
+          ),
           headerRight: () => (
             <Pressable
               onPress={() => navigation.navigate('Modal')}
               style={({ pressed }) => ({
                 opacity: pressed ? 0.5 : 1,
-              })}>
+              })}
+            >
               <FontAwesome
-                name="info-circle"
+                name='info-circle'
                 size={25}
-                color={Colors[colorScheme].text}
+                color={Colors.text}
                 style={{ marginRight: 15 }}
               />
             </Pressable>
@@ -85,11 +86,11 @@ function BottomTabNavigator() {
         })}
       />
       <BottomTab.Screen
-        name="TabTwo"
+        name='TabTwo'
         component={TabTwoScreen}
         options={{
           title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
         }}
       />
     </BottomTab.Navigator>
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,66 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import * as Location from 'expo-location';
+import { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import opencage from 'opencage-api-client';
 import { RootTabScreenProps } from '../types';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<'TabOne'>) {
+  const [loaction, setLocation] = useState<any>();
+  const [errorMsg, setErrorMsg] = useState<string>('');
+  const [address, setAddress] = useState<string>('');
+
+  let url;
+
+  const reverseGeocode = async (lat: any, long: any) => {
+    const key = '2041e9fcb53f4c1c99f231515f76d559';
+    const response = await opencage.geocode({
+      key,
+      q: `${lat}, ${long}`,
+    });
+
+    const result = response.results[0];
+    console.log(result.formatted);
+    setAddress(result.formatted);
+  };
+
+  const fetchLocation = useCallback(async () => {
+    const response = await Location.requestForegroundPermissionsAsync();
+
+    // console.log(response, 'response');
+
+    if (response.status !== 'granted') {
+      setErrorMsg('Permission to access loacation was denied');
+    }
+    const Myloaction = await Location.getCurrentPositionAsync();
+
+    setLocation(Myloaction.coords.latitude);
+
+    url = `https://api.opencagedata.com/geocode/v1/json?q=${
+      (Myloaction.coords.latitude, Myloaction.coords.longitude)
+    }&key=2041e9fcb53f4c1c99f231515f76d559&language=en`;
+
+    console.log(url, 'Punith');
+
+    reverseGeocode(Myloaction.coords.latitude, Myloaction.coords.longitude);
+  }, [loaction, errorMsg]);
+
+  useEffect(() => {
+    let interval: any = setInterval(() => {
+      console.log('calling function');
+      fetchLocation();
+    }, 3_00_000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>{address ? address : ''}</Text>
+      <View style={styles.separator} />
     </View>
   );
 }
